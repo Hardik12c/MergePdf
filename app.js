161f55db15be351d2272aff9f344ddae5db5a259
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2022-12-17 02:37:08 +0530

    automatic file deletion implemented

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express =require('express');
 const multer  = require('multer');
 const path = require('path');
+const fs = require('fs');
 const mergepdfs=require('./routes/pdf-merger');
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: 'uploads/' });
+
+var folder = './uploads/';  //folder directory
+
 const app=express();
-app.use('/static', express.static('public'))
+
+
+app.use('/static', express.static('public'))   // to redirect to pdf 
 
 app.use(express.json());
-app.use(express.static('./public'))
+app.use(express.static('./public'))  // to serve basic index.html
 
 
 app.post('/merge', upload.array('pdfs', 12), async(req, res, next) =>{
     try {
         let d=await mergepdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path));
+        // To remove files from uploads folder we are using this function to get storage efficiency
+        fs.readdir(folder, (err, files) => {
+            if (err) throw err;
+            for (const file of files) {
+                fs.unlinkSync(folder+file);
+            }
+        });
         res.status(201).json({data:d,message:"success"});
     } catch (error) {
         console.log(error);
